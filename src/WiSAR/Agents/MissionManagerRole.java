a8beb1679eee020aa9afa3a4f957ea6183731ce8
erconui <erconui@gmail.com>
2013-04-26 20:45:05 -0600

    Put the MissionManager in a format meeting current specifications. Parts left incomplete are indicated with TODO comments.

--- src/WiSAR/Agents/MissionManagerRole.java
+++ src/WiSAR/Agents/MissionManagerRole.java
@@ -20,8 +20,8 @@ public class MissionManagerRole extends Actor {
 		/**
 		 * Operator and VideoOperator
 		 */
-		MM_SEARCH_AOI,
-		MM_SEARCH_TERMINATED,
+		SEARCH_AOI_MM,
+		SEARCH_TERMINATED_MM,
 		
 		/**
 		 * Video Operator
@@ -37,7 +37,8 @@ public class MissionManagerRole extends Actor {
 		/**
 		 * ParentSearch
 		 */
-		SEARCH_AOI_COMPLETE,
+		SEARCH_AOI_COMPLETE_MM,
+		FOUND_ANOMALY_MM,
 		
 		/**
 		 * global outputs
@@ -54,14 +55,22 @@ public class MissionManagerRole extends Actor {
 	{
 		IDLE,
 		POKE_PS,
-//		MM_POKE_OPERATOR,
+		POKE_OP,
+		POKE_VO,
 		TX_PS,
-//		MM_TX_OPERATOR,
+		TX_OP,
+		TX_VO,
 		END_PS,
-//		MM_END_OPERATOR,
+		END_OP,
+		END_VO,
 //		MM_ACK_PS,
 //		MM_ACK_OPERATOR,
 		RX_PS,
+		RX_VO,
+		RX_OP,
+		ACK_PS,
+		ACK_VO,
+		ACK_OP,
 //		MM_RX_OPERATOR
 	}
 	
@@ -96,7 +105,16 @@ public class MissionManagerRole extends Actor {
 				nextState(null, 0);
 				break;
 			case POKE_PS:
-				sim().addInput(Roles.PARENT_SEARCH.name(), Outputs.POKE_PS);
+				sim().addOutput(Roles.PARENT_SEARCH.name(), Outputs.POKE_MM);
+				//TODO store the message to be sent in END_PS
+				nextState(States.IDLE, sim().duration(Durations.MM_POKE_DUR.range()));
+				break;
+			case POKE_OP:
+				sim().addOutput(Roles.OPERATOR.name(), Outputs.POKE_MM);
+				nextState(States.IDLE, sim().duration(Durations.MM_POKE_DUR.range()));
+				break;
+			case POKE_VO:
+				sim().addOutput(Roles.VIDEO_OPERATOR.name(), Outputs.POKE_MM);
 				nextState(States.IDLE, sim().duration(Durations.MM_POKE_DUR.range()));
 				break;
 			case TX_PS:
@@ -105,15 +123,34 @@ public class MissionManagerRole extends Actor {
 //				nextState(States.END_PS, simulator().duration(Durations.MM_TX_SEARCH_FAILED_PS_DUR.range()));
 				nextState(States.END_PS, sim().duration(Durations.MM_TX_SEARCH_COMPLETE_PS_DUR.range()));
 				break;
+			case TX_OP:
+				//TODO change this duration based on data transmitted
+				nextState(States.END_OP, 1);
+				break;
+			case TX_VO:
+				//TODO change this duration based on data transmitted
+				nextState(States.END_VO, 1);
+				break;
 			case END_PS:
 				//Send the Data and End Msg and move into an idle state
-				sim().addInput(Roles.PARENT_SEARCH.name(), Outputs.SEARCH_AOI_COMPLETE);
-				sim().addInput(Roles.PARENT_SEARCH.name(), Outputs.END_PS);
+				sim().addOutput(Roles.PARENT_SEARCH.name(), Outputs.SEARCH_AOI_COMPLETE_MM);
+				sim().addOutput(Roles.PARENT_SEARCH.name(), Outputs.END_MM);
+				nextState(States.IDLE, 1);
+				break;
+			case END_OP:
+				//TODO output based off of what the PS instructions are
+				//TODO if message to be forwarded to the VO then nextState POKE_VO
+				nextState(States.IDLE, 1);
+				break;
+			case END_VO:
+				//TODO output based off of what the PS instructions are
 				nextState(States.IDLE, 1);
 				break;
 			case RX_PS:
 				nextState(States.IDLE, sim().duration(Durations.MM_RX_DUR.range()));
 				break;
+			case RX_VO:
+				
 			default:
 				//Stay as we are
 				nextState(null, 0);
@@ -128,14 +165,15 @@ public class MissionManagerRole extends Actor {
 //		temp_inputs.addAll(_input);
 //		_input.clear();
 		switch( (States) state() ) {
+		
 			case IDLE:
 				//If the MM is idle then do the following things in sequence
 				//First check for Parent Search Commands
 				if ( _input.contains(ParentSearch.Outputs.POKE_PS) ) {
-					sim().addInput(Roles.PARENT_SEARCH.name(), Outputs.ACK_PS);
+					sim().addOutput(Roles.PARENT_SEARCH.name(), Outputs.ACK_MM);
 					nextState(States.RX_PS, 1);
-				}
-				
+				} 
+				//TODO Handle pokes from the OPERATOR and the VIDEO_OPERATOR.
 				//TODO Handle more input values
 				break;
 			case POKE_PS:
@@ -159,9 +197,13 @@ public class MissionManagerRole extends Actor {
 				if ( _input.contains(ParentSearch.Outputs.END_PS) ) {
 					//Look for the inputs
 					if ( _input.contains(ParentSearch.Outputs.SEARCH_TERMINATED) ) {
-						//TODO Tell the OPERATOR and the VA
+						//TODO Tell the OPERATOR and the VO
+						//TODO Store the message to be sent to the OPERATOR and VideoOperator
+						nextState(States.POKE_OP, 1);
 					} else if ( _input.contains(ParentSearch.Outputs.SEARCH_AOI) ) {
 						//TODO Send AOI to the OPERATOR
+						//TODO store desired message to send OPERATOR
+						nextState(States.POKE_OP,1);
 					}
 					
 					nextState(States.IDLE, 1);
