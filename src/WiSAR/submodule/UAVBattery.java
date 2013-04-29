dfd9e948e71f528590bc688086aea265154ac7fa
erconui <erconui@gmail.com>
2013-04-29 12:16:18 -0600

    Battery state machine.

---
+++ src/WiSAR/submodule/UAVBattery.java
@@ -0,0 +1,85 @@
+package WiSAR.subAgents;
+
+import CUAS.Simulator.Actor;
+import CUAS.Simulator.IData;
+import CUAS.Simulator.IStateEnum;
+import CUAS.Simulator.Simulator;
+import WiSAR.Durations;
+import WiSAR.Agents.Roles;
+import WiSAR.Agents.UAVRole;
+
+
+public class UAVBattery extends Actor {
+
+	private int start_time = 0;
+	private int battery_life = 0;
+	private int low_battery_life = 0;
+	
+	public enum Outputs implements IData {
+		DEAD_BATTERY,
+		LOW_BATTERY,
+		OFF,
+		OK
+		
+	}
+	public enum States implements IStateEnum{
+		OFF,
+		OK,
+		LOW,
+		DEAD
+	}
+	@Override
+	public void processNextState() {
+
+        if ( nextStateTime() != Simulator.getInstance().getTime() ) {
+            return;
+        }
+        state(nextState());
+        int duration = 1;
+        //If a state isn't included then it doesn't deviate from the default
+        switch((States)nextState()) {
+        case OFF:
+        	_output.add(Outputs.OFF);
+        case OK:
+        	start_time = sim().getTime();
+        	battery_life = sim().duration(Durations.UAV_BATTERY_DUR.range());
+        	low_battery_life = sim().duration(Durations.UAV_LOW_BATTERY_THRESHOLD_DUR.range());
+        	duration = battery_life - low_battery_life;
+        	_output.add(Outputs.OK);
+        	nextState(States.LOW, duration);
+        	break;
+        case LOW:
+        	duration = low_battery_life;
+        	_output.add(Outputs.LOW_BATTERY);
+        	nextState(States.DEAD,duration);
+        	break;
+        case DEAD:
+        	_output.add(Outputs.DEAD_BATTERY);
+    	default:
+    		nextState(null,0);
+    		break;
+        }
+	}
+
+	@Override
+	public void processInputs() {
+		switch((States)state()){
+		case OFF:
+			if(_input.contains(UAVRole.Outputs.ACTIVATE_BATTERY)){
+				nextState(States.OK,1);
+			}
+			break;
+		case OK:
+		case LOW:
+			if(_input.contains(UAVRole.Outputs.DEACTIVATE_BATTERY)){
+				nextState(States.OFF,1);
+			}
+		default:
+		}
+	}
+	
+	public int getRemainingTime(){
+		return (start_time + battery_life) - sim().getTime();
+	}
+
+}
