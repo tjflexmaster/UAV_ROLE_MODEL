4e02d2981694e363da3d159e1f5e3c0cc837af94
tj <tjflexmaster@gmail.com>
2013-04-30 16:25:07 -0600

    Team now allows for the registration of child Actors.
    This will be obsolete soon.

--- src/WiSAR/DefaultTeam.java
+++ src/WiSAR/DefaultTeam.java
@@ -9,12 +9,12 @@ import WiSAR.Agents.ParentSearch;
 import WiSAR.Agents.UAVRole;
 import WiSAR.Agents.VideoGUIRole;
 import WiSAR.Agents.VideoOperatorRole;
-import WiSAR.submodule.UAVBattery;
 
 public class DefaultTeam extends Team {
 
 	public DefaultTeam()
 	{
+		//Add main actors
 		_actors.put(Actors.PARENT_SEARCH.name(), new ParentSearch());
 		_actors.put(Actors.MISSION_MANAGER.name(), new MissionManagerRole());
 		_actors.put(Actors.OPERATOR.name(), new OperatorRole());
@@ -22,7 +22,15 @@ public class DefaultTeam extends Team {
 		_actors.put(Actors.VIDEO_OPERATOR.name(), new VideoOperatorRole());
 		_actors.put(Actors.VIDEO_OPERATOR_GUI.name(), new VideoGUIRole());
 		_actors.put(Actors.UAV.name(), new UAVRole());
-		_actors.put(Actors.UAV_BATTERY.name(), new UAVBattery());
+		
+//		//Register Child Actors so their inputs and outputs work correctly
+//		registerChildActor(Actors.UAV.name(), Actors.UAV_BATTERY.name());
+//		registerChildActor(Actors.UAV.name(), Actors.UAV_FLIGHT_PLAN.name());
+//		registerChildActor(Actors.UAV.name(), Actors.UAV_HAG.name());
+//		registerChildActor(Actors.UAV.name(), Actors.UAV_PATH.name());
+//		registerChildActor(Actors.UAV.name(), Actors.UAV_SIGNAL.name());
+		
+//		_actors.put(Actors.UAV_BATTERY.name(), new UAVBattery());
 //		_roles.put(RoleType.ROLE_ENVIRONMENT, new EnvironmentRole());
 //		_roles.put(RoleType.ROLE_UAV, new UAVRole());
 //		_roles.put(RoleType.ROLE_UAV_GUI, new UAVGUIRole());
