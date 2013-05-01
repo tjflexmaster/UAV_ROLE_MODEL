f2b0afc510a2cf9ab39b4f94e1d34b5daaf8cbd2
erconui <erconui@gmail.com>
2013-05-01 09:43:59 -0600

    Made the video Operator implement a priority queue for next task to execute and to return to idle between receiving instructions and acting on them.

--- src/WiSAR/Agents/VideoOperatorRole.java
+++ src/WiSAR/Agents/VideoOperatorRole.java
@@ -12,6 +12,7 @@ import CUAS.Simulator.IStateEnum;
 import CUAS.Simulator.Simulator;
 import WiSAR.Actors;
 import WiSAR.Durations;
+import java.util.PriorityQueue;
 
 /**
  *
@@ -21,7 +22,8 @@ import WiSAR.Durations;
 public class VideoOperatorRole extends Actor
 {
 	ArrayList<IData> memory = new ArrayList<IData>();
-	   
+	PriorityQueue<IData> tasks;
+	
 	public enum Outputs implements IData
 	{
 		VO_POKE,
@@ -78,7 +80,7 @@ public class VideoOperatorRole extends Actor
 	{
 		name( Actors.VIDEO_OPERATOR.name() );
 		nextState(States.IDLE, 1);
-		
+		tasks = new PriorityQueue<IData>();
 	}
 	
 	/**
@@ -102,6 +104,7 @@ public class VideoOperatorRole extends Actor
 
         //If a state isn't included then it doesn't deviate from the default
         //ICommunicate role; 
+        IData output;
         //TODO handle outputing BUSY_VO
         switch((States)nextState()) {
 	        case ACK_MM:
@@ -136,11 +139,11 @@ public class VideoOperatorRole extends Actor
 	        	break;
 	        case TX_OPERATOR:
 	        	int duration = 1;
-	        	if(memory.contains(Outputs.VO_BAD_STREAM))
+	        	if(tasks.peek() == Outputs.VO_BAD_STREAM)
 	        		duration = sim().duration(Durations.VO_TX_OPERATOR_BAD_STREAM.range());
-	        	else if(memory.contains(Outputs.VO_STREAM_ENDED))
+	        	else if(tasks.peek() == Outputs.VO_STREAM_ENDED)
 	        		duration = sim().duration(Durations.VO_TX_OPERATOR_STREAM_ENDED.range());
-	        	else if(memory.contains(Outputs.VO_LOOK_CLOSER))
+	        	else if(tasks.peek() == Outputs.VO_LOOK_CLOSER)
 	        		duration = sim().duration(Durations.VO_TX_OPERATOR_LOOK_CLOSER.range());
 	        	else{
 	        		//TODO handle more outputs
@@ -149,35 +152,70 @@ public class VideoOperatorRole extends Actor
 	        	break;
 	        //For all the end transmission states update the inputs to the receivers then return to observation.
 	        case END_GUI:
-	        	if(memory.contains(Outputs.VO_CLICK_FRAME))
-	        		sim().addOutput(Actors.VIDEO_OPERATOR_GUI.name(), Outputs.VO_CLICK_FRAME);
-	        	else if(memory.contains(Outputs.VO_START_FEED))
-	        		sim().addOutput(Actors.VIDEO_OPERATOR_GUI.name(), Outputs.VO_START_FEED);
-	        	else if(memory.contains(Outputs.VO_END_FEED))
-	        		sim().addOutput(Actors.VIDEO_OPERATOR_GUI.name(), Outputs.VO_END_FEED);
+	        	output = tasks.poll();
+	        	if(output == Outputs.VO_CLICK_FRAME)
+	        		sim().addOutput(Actors.VIDEO_OPERATOR_GUI.name(), output);
+	        	else if(output == Outputs.VO_START_FEED)
+	        		sim().addOutput(Actors.VIDEO_OPERATOR_GUI.name(), output);
+	        	else if(output == Outputs.VO_END_FEED)
+	        		sim().addOutput(Actors.VIDEO_OPERATOR_GUI.name(), output);
 	        	else{
 	        		//TODO handle different outputs
 	        	}
 	        	nextState(States.OBSERVING,1);
 	        	break;
 	        case END_MM:
+	        	output = tasks.poll();
 	        	sim().addOutput(Actors.MISSION_MANAGER.name(), Outputs.VO_END);
-	        	if(memory.contains(Outputs.VO_FOUND_ANOMALY))
-	        		sim().addOutput(Actors.MISSION_MANAGER.name(), Outputs.VO_FOUND_ANOMALY);
+	        	if(output == Outputs.VO_FOUND_ANOMALY)
+	        		sim().addOutput(Actors.MISSION_MANAGER.name(), output);
 	        	nextState(States.OBSERVING,1);
 	        	break;
 	        case END_OPERATOR:
 	        	//TODO implement comm with OPERATOR
-	        	/**
-	        	if(memory.contains(Outputs.BAD_STREAM))
-	        		simulator().addOutput(Actors.OPERATOR.name(), OPERATORRole.Inputs.BAD_STREAM);
-	        	else if(memory.contains(Outputs.STREAM_ENDED))
-	        		simulator().addOutput(Actors.OPERATOR.name(), OPERATORRole.Inputs.STREAM_ENDED);
-	        	else if(memory.contains(Outputs.LOOK_CLOSER))
-	        		simulator().addOutput(Actors.OPERATOR.name(), OPERATORRole.Inputs.LOOK_CLOSER);
-	        		*/
+	        	output = tasks.poll();
+	        	if(output == Outputs.VO_BAD_STREAM)
+	        		sim().addOutput(Actors.OPERATOR.name(), output);
+	        	else if(output == Outputs.VO_STREAM_ENDED)
+	        		sim().addOutput(Actors.OPERATOR.name(), output);
+	        	else if(output == Outputs.VO_LOOK_CLOSER)
+	        		sim().addOutput(Actors.OPERATOR.name(), output);
 	        	nextState(States.OBSERVING,1);
 	        	break;
+	        case IDLE:
+	        	IData current_task = tasks.peek();
+	        	if(current_task == null){
+					nextState(null,0);
+					break;
+	        	}
+	        	switch((Outputs)current_task){
+	    		/**
+	    		 * For the GUI
+	    		 */
+	        	case VO_GET_DATA:
+	        	case VO_CLICK_FRAME:
+	        	case VO_END_FEED:
+	        	case VO_START_FEED:
+	        		nextState(States.POKE_GUI,1);
+	        		break;
+	    		/**
+	    		 * For the OPERATOR
+	    		 */
+	        	case VO_LOOK_CLOSER:
+	        	case VO_BAD_STREAM:
+	        	case VO_STREAM_ENDED:
+	        		nextState(States.POKE_OPERATOR,1);
+	        		break;
+	    		/**
+	    		 * For the MM
+	    		 */
+	        	case VO_FOUND_ANOMALY: 
+	        		nextState(States.POKE_MM,1);
+	        		break;
+				default:
+					nextState(null,0);
+					break;
+	        	}
 	        default:
 	        	nextState(null,0);
 	        	break;
@@ -202,16 +240,15 @@ public class VideoOperatorRole extends Actor
 				{
 					if(input.contains(MissionManagerRole.Outputs.MM_SEARCH_AOI))
 					{
-						memory.add(Outputs.VO_START_FEED);
-						nextState(States.POKE_GUI,1);
+						//memory.add(Outputs.VO_START_FEED);
+						tasks.add(Outputs.VO_START_FEED);
 					}
 					else if(input.contains(MissionManagerRole.Outputs.MM_SEARCH_TERMINATED))
 					{
-						memory.add(Outputs.VO_END_FEED);
-						nextState(States.POKE_GUI,1);
+						//memory.add(Outputs.VO_END_FEED);
+						tasks.add(Outputs.VO_END_FEED);
 					}
-					else
-						nextState(States.IDLE,1);
+					nextState(States.IDLE,1);
 				}
 				break;
 		//Check the inputs from the GUI and if the MM has initiated a handshake.
@@ -219,28 +256,34 @@ public class VideoOperatorRole extends Actor
 				video_feed = sim().getObservations(Actors.VIDEO_OPERATOR_GUI.name());
 				if(video_feed.contains(VideoGUIRole.Outputs.VGUI_BAD_STREAM))
 				{
-					memory.add(Outputs.VO_BAD_STREAM);
-					nextState(States.POKE_OPERATOR, 1);
+					//memory.add(Outputs.VO_BAD_STREAM);
+					tasks.add(Outputs.VO_BAD_STREAM);
+					//nextState(States.POKE_OPERATOR, 1);
 				} 
 				else if (video_feed.contains(VideoGUIRole.Outputs.VGUI_BAD_STREAM))
 				{
-					memory.add(Outputs.VO_STREAM_ENDED);
-					nextState(States.POKE_OPERATOR, 1);
+					tasks.add(Outputs.VO_STREAM_ENDED);
+					//memory.add(Outputs.VO_STREAM_ENDED);
+					//nextState(States.POKE_OPERATOR, 1);
 				}
-				else if (input.contains(MissionManagerRole.Outputs.MM_ACK))
+				else if (input.contains(MissionManagerRole.Outputs.MM_POKE))
 				{
-					nextState(States.ACK_MM,1);
+					sim().addOutput(Actors.MISSION_MANAGER.name(), Outputs.VO_ACK);
+					nextState(States.RX_MM,1);
 				}
 				else if(video_feed.contains(VideoGUIRole.Outputs.VGUI_TRUE_POSITIVE))
 				{
-					memory.add(Outputs.VO_FOUND_ANOMALY);
-					nextState(States.POKE_GUI,1);
+					tasks.add(Outputs.VO_FOUND_ANOMALY);
+					//memory.add(Outputs.VO_FOUND_ANOMALY);
+					//nextState(States.POKE_GUI,1);
 				}
 				else if(video_feed.contains(VideoGUIRole.Outputs.VGUI_FALSE_POSITIVE))
 				{
-					memory.add(Outputs.VO_FOUND_ANOMALY);
-					nextState(States.POKE_GUI,1);
+					tasks.add(Outputs.VO_FOUND_ANOMALY);
+					//memory.add(Outputs.VO_FOUND_ANOMALY);
+					//nextState(States.POKE_GUI,1);
 				}
+				nextState(States.OBSERVING,1);
 				break;
 			case POKE_GUI:
 				video_feed = sim().getObservations(Actors.VIDEO_OPERATOR_GUI.name());
