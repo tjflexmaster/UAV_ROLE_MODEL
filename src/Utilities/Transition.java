045cd190a6ada56780d92749ea5207427dbbadeb
erconui <erconui@gmail.com>
2013-06-07 19:59:32 -0600

    updated the transitions method

--- src/Utilities/Transition.java
+++ src/Utilities/Transition.java
@@ -1,64 +1,88 @@
 package Utilities;
 
+import java.util.ArrayList;
+
 
 /**
  * @author rob.ivie
  */
 public class Transition {
 	
-	public State _startState;
-	public Data _input;
-	public int _time;
-	public State _nextState;
-	public Data _output;
+	//public State _startState;
+	private ArrayList<UDO> _inputs;
+	private int _duration;
+	private State _endState;
+	private ArrayList<UDO> _outputs;
+	private State _curState;
 
 	/**
 	 * a transition is used by an agent (state machine) to formally define state transitions 
-	 * @param startState the state an agent must be in before it can make the transition
 	 * @param input the necessary input an agent needs before it can make the transition
-	 * @param nextState the new state the agent will move to
+	 * @param endState the new state the agent will move to
 	 * @param output the output the transition produces
+	 * @param curState the current state of the actor
 	 * todo add a duration that represents how long it takes to move between states
 	 */
-	public Transition (State startState, Data input, State nextState, Data output) {
+	public Transition (ArrayList<UDO> inputs, ArrayList<UDO> outputs, State endState, State curState, int duration) {
 		
-		_startState = startState;
-		_input = input;
-		_nextState = nextState;
-		_output = output;
+		_inputs = inputs;
+		_outputs = outputs;
+		_endState = endState;
+		_curState = curState;
+		_duration = duration;
 		
 	}
 
-	/**
-	 * a transition is used by an agent (state machine) to formally define state transitions 
-	 * @param startState the state an agent must be in before it can make the transition
-	 * @param time the necessary input an agent needs before it can make the transition
-	 * @param nextState the new state the agent will move to
-	 * @param output the output the transition produces
-	 * todo add a duration that represents how long it takes to move between states
-	 */
-	public Transition(State startState, int time, State nextState, Data output) {
-		
-		_startState = startState;
-		_time = time; 
-		_nextState = nextState;
-		_output = output;
-		
+//	/**
+//	 * a transition is used by an agent (state machine) to formally define state transitions 
+//	 * @param startState the state an agent must be in before it can make the transition
+//	 * @param time the necessary input an agent needs before it can make the transition
+//	 * @param nextState the new state the agent will move to
+//	 * @param output the output the transition produces
+//	 * todo add a duration that represents how long it takes to move between states
+//	 */
+//	public Transition(int time, State nextState, ArrayList<UDO> outputs) {
+//		
+//		
+//		_time = time; 
+//		_nextState = nextState;
+//		_outputs = outputs;
+//		
+//	}
+	
+	public boolean isPossible(){
+		for(UDO input : _inputs){
+			if(!input.isActive()){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public State process(){
+		for(UDO output : _outputs){
+			output.setActive();
+		}
+		return _endState;
+	}
+	
+	public int getDuration(){
+		return _duration;
 	}
 	
 	public String toString() {
 		
-		if ( _input == null ) {
+		if ( _inputs == null ) {
 			
-			return _startState.toString() + " X " + Integer.toString(_time)
-					+ " -> " + _nextState.toString() + " X " + _output.toString();
+			return " X " + Integer.toString(_duration)
+					+ " -> " + _endState.toString() + " X " + _outputs.toString();
 			
 		}
 		
 		else {
 		
-			return _startState.toString() + " X " + _input.toString()
-					+ " -> " + _nextState.toString() + " X " + _output.toString();
+			return " X " + _inputs.toString()
+					+ " -> " + _endState.toString() + " X " + _outputs.toString();
 			
 		}
 		
