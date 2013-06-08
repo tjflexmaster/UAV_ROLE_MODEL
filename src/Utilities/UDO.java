0816690b624ed42b01c61062ba88a0f458821062
erconui <erconui@gmail.com>
2013-06-07 19:57:21 -0600

    added the UDO class

---
+++ src/Utilities/UDO.java
@@ -0,0 +1,29 @@
+package Utilities;
+
+public class UDO {
+
+	private boolean active;
+	private boolean temp;
+	private String name;
+	
+	public UDO(String _name){
+		name = _name;
+		active = false;
+		temp = false;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive() {
+		temp = true;
+	}
+	
+	
+	public void processData(){
+		active = temp;
+		temp = false;
+	}
+	
+}
