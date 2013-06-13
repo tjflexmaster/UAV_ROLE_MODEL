910e55a0bd8c008f31dc8392e1335f5e86fb9380
rob.ivie <r.p.ivie@gmail.com>
2013-06-12 18:49:26 -0600

    Changed clock variable inside delta clock to actors, since it is a list of actors.

--- src/Simulator/DeltaClock.java
+++ src/Simulator/DeltaClock.java
@@ -12,7 +12,7 @@ public class DeltaClock {
 	 * 		this list will be reordered by actor.nextTime as a delta clock is ordered 
 	 * @param 
 	 */
-	public void run(ActorsList clock) {
+	public void run(ActorsList actors) {
 		
 		//the scanner is used to communicate with the user running the simulation
 		Scanner scanner = new Scanner(System.in);
@@ -22,31 +22,31 @@ public class DeltaClock {
 		do {
 			
 			//update actors next planned transition (currentTransition)
-			for (int clockIndex = 0; clockIndex < clock.size(); clockIndex++) {
+			for (int actorsIndex = 0; actorsIndex < actors.size(); actorsIndex++) {
 				
-				if (clock.get(clockIndex).updateTransition(currentTime)) {
-					clock = resetClock(clock, clock.get(clockIndex));
+				if (actors.get(actorsIndex).updateTransition(currentTime)) {
+					actors = resetClock(actors, actors.get(actorsIndex));
 				}
 				
 			}
 			
 			//tick the clock until a transition happens
-			currentTime += clock.get(0).getNextTime();
-			clock.get(0).setNextTime(0);
+			currentTime += actors.get(0).getNextTime();
+			actors.get(0).setNextTime(0);
 			
 			//communicate with the user
 			communicate(currentTime);
 			
 			//process all actors that have a next time equal to zero
-			for (int clockIndex = 0; clockIndex < clock.size(); clockIndex++) {
+			for (int actorsIndex = 0; actorsIndex < actors.size(); actorsIndex++) {
 				
-				if (clock.get(clockIndex).processTransition()) {
-					clock = resetClock(clock, clock.get(clockIndex));
+				if (actors.get(actorsIndex).processTransition()) {
+					actors = resetClock(actors, actors.get(actorsIndex));
 				}
 				
 			}
 			
-		} while (isRunning(clock));
+		} while (isRunning(actors));
 		
 		//close the scanner once the simulation is complete
 		scanner.close();
@@ -59,32 +59,32 @@ public class DeltaClock {
 	 * @param actor
 	 * @return 
 	 */
-	private static ActorsList resetClock(ActorsList clock, Actor actor) {
-		clock.remove(actor);
+	private static ActorsList resetClock(ActorsList actors, Actor actor) {
+		actors.remove(actor);
 		
-		for (int clockIndex = 0; clockIndex < clock.size(); clockIndex++) {
+		for (int actorsIndex = 0; actorsIndex < actors.size(); actorsIndex++) {
 			//if this spot in the list (delta clock) is empty place the actor here
-			if (clock.get(clockIndex).getNextTime() == -1) {
-				clock.add(actor);
+			if (actors.get(actorsIndex).getNextTime() == -1) {
+				actors.add(actor);
 				break;
 			}
 			//if the actor has less time then place it here and update next actor's nextTime
-			else if (clock.get(clockIndex).getNextTime() > actor.getNextTime()) {
-				clock.get(clockIndex).setNextTime(clock.get(clockIndex).getNextTime() - actor.getNextTime());
-				clock.add(clockIndex, actor);
+			else if (actors.get(actorsIndex).getNextTime() > actor.getNextTime()) {
+				actors.get(actorsIndex).setNextTime(actors.get(actorsIndex).getNextTime() - actor.getNextTime());
+				actors.add(actorsIndex, actor);
 				break;
 			}
 			//if the actor has more time, then update its time and move to the next space
-			else if (clock.get(clockIndex).getNextTime() < actor.getNextTime()) {
-				actor.setNextTime(actor.getNextTime() - clock.get(clockIndex).getNextTime());
+			else if (actors.get(actorsIndex).getNextTime() < actor.getNextTime()) {
+				actor.setNextTime(actor.getNextTime() - actors.get(actorsIndex).getNextTime());
 			}
 			//if the list (delta clock) is full, then add actor to the end 
-			else if (clockIndex == clock.size() - 1) {
-				clock.add(actor);
+			else if (actorsIndex == actors.size() - 1) {
+				actors.add(actor);
 			}
 		}
 		
-		return clock;
+		return actors;
 	}
 	
 	/**
@@ -101,11 +101,11 @@ public class DeltaClock {
 	 * @param clock
 	 * @return
 	 */
-	private static boolean isRunning(ArrayList<Actor> clock) {
+	private static boolean isRunning(ArrayList<Actor> actors) {
 		
 		boolean result = false;
-		for (int clockIndex = 0; clockIndex < clock.size(); clockIndex++) {
-			if (clock.get(clockIndex).getNextTime() != -1) {
+		for (int actorsIndex = 0; actorsIndex < actors.size(); actorsIndex++) {
+			if (actors.get(actorsIndex).getNextTime() != -1) {
 				result = true;
 			}
 		}
---
+++ src/Simulator/Duration.java
@@ -0,0 +1,20 @@
+package Simulator;
+
+public class Duration {
+	
+	int _minimum;
+	int _maximum;
+
+	public Duration(int minimum, int maximum) {
+		
+		_minimum = minimum;
+		_maximum = maximum;
+		
+	}
+
+	public String getMaximum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
