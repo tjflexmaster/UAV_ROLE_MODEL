1486461552346a3950f06b5b1dbe96e0f79b596b
rob <ivier@studentbody.byu.edu>
2013-07-30 16:33:58 -0600

    added null checks to the predicates

--- src/simulator/EqualPredicate.java
+++ src/simulator/EqualPredicate.java
@@ -16,6 +16,8 @@ public class EqualPredicate<T extends Comparable<? super T>> implements IPredica
 	@Override
 	public boolean evaluate(Object right) {
 		assert _left.getClass() == right.getClass() : "Data is of different type.";
+		if (_left == null || right == null)
+			return false;
 		if (((T) _left).compareTo((T) right) == 0)
 			return true;
 		return false;
