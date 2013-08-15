17ac7fda38ecbb943361f109efad8f22ccac08b5
rob <ivier@studentbody.byu.edu>
2013-08-15 17:31:36 -0600

    cleaned up the metric in the vo watered down actor

--- src/model/actors/VO_WateredDown.java
+++ src/model/actors/VO_WateredDown.java
@@ -28,21 +28,24 @@ public class VO_WateredDown extends Actor {
 		IDLE.add(new Transition(_internal_vars,inputs,outputs,IDLE){//(IDLE, [], [])->(IDLE , [], [])
 			@Override
 			public boolean isEnabled(){
-				if(MissionManager.AUDIO_MM_VO_COMM.MM_POKE_VO.equals(_inputs.get(Channels.AUDIO_MM_VO_COMM.name()).value())){
+				Object AUDIO_MM_VO_COMM = _inputs.get(Channels.AUDIO_MM_VO_COMM.name()).value();
+				Integer NEW_TARGET_SIGHTED_F = (Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_F");
+				Integer NEW_TARGET_SIGHTED_T = (Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_T");
+				if(MissionManager.AUDIO_MM_VO_COMM.MM_POKE_VO.equals(AUDIO_MM_VO_COMM)){
 					this.setTempOutput(Channels.AUDIO_VO_MM_COMM.name(), VideoOperator.AUDIO_VO_MM_COMM.VO_ACK_MM);
 					return true;
-				} else if(MissionManager.AUDIO_MM_VO_COMM.MM_TARGET_DESCRIPTION.equals(_inputs.get(Channels.AUDIO_MM_VO_COMM.name()).value())){
+				} else if(MissionManager.AUDIO_MM_VO_COMM.MM_TARGET_DESCRIPTION.equals(AUDIO_MM_VO_COMM)){
 					this.setTempOutput(Channels.AUDIO_VO_MM_COMM.name(), null);
 					this.setTempInternalVar("TARGET_DESCRIPTION", "CURRENT");
 					return true;
-				} else if(MissionManager.AUDIO_MM_VO_COMM.MM_TERMINATE_SEARCH.equals(_inputs.get(Channels.AUDIO_MM_VO_COMM.name()).value())){
+				} else if(MissionManager.AUDIO_MM_VO_COMM.MM_TERMINATE_SEARCH.equals(AUDIO_MM_VO_COMM)){
 					this.setTempInternalVar("TARGET_DESCRIPTION", null);
 					this.setTempOutput(Channels.AUDIO_VO_MM_COMM.name(), null);
-				} else if(((Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_F")) == 1 && MissionManager.AUDIO_MM_VO_COMM.MM_ACK_VO.equals(_inputs.get(Channels.AUDIO_MM_VO_COMM.name()).value())){
+				} else if(NEW_TARGET_SIGHTED_F == 1 && MissionManager.AUDIO_MM_VO_COMM.MM_ACK_VO.equals(AUDIO_MM_VO_COMM)){
 					this.setTempInternalVar("NEW_TARGET_SIGHTED_F", 2);//advance
 					this.setTempOutput(Channels.AUDIO_VO_MM_COMM.name(), VideoOperator.AUDIO_VO_MM_COMM.VO_TARGET_SIGHTED_F);//(IDLE, [MM_ACK_VO], [])->(IDLE, [VO_TARGET_SIGHTED_F], [])
 					return true;
-				} else if(((Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_T")) == 1 && MissionManager.AUDIO_MM_VO_COMM.MM_ACK_VO.equals(_inputs.get(Channels.AUDIO_MM_VO_COMM.name()).value())){
+				} else if(NEW_TARGET_SIGHTED_T == 1 && MissionManager.AUDIO_MM_VO_COMM.MM_ACK_VO.equals(AUDIO_MM_VO_COMM)){
 					this.setTempInternalVar("NEW_TARGET_SIGHTED_T", 2);//advance
 					this.setTempOutput(Channels.AUDIO_VO_MM_COMM.name(), VideoOperator.AUDIO_VO_MM_COMM.VO_TARGET_SIGHTED_T);//(IDLE, [MM_ACK_VO], [])->(IDLE, [VO_TARGET_SIGHTED_T], [])
 					return true;
@@ -52,7 +55,10 @@ public class VO_WateredDown extends Actor {
 		});IDLE.add(new Transition(_internal_vars,inputs,outputs,IDLE, Duration.RANDOM.getRange()){//(IDLE, [], [])->(IDLE , [], [])
 			@Override
 			public boolean isEnabled(){
-				if("CURRENT".equals(this._internal_vars.getVariable("TARGET_DESCRIPTION")) &&((Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_F")) == 0 && ((Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_T")) == 0) {
+				Object TARGET_DESCRIPTION = this._internal_vars.getVariable("TARGET_DESCRIPTION");
+				Integer NEW_TARGET_SIGHTED_F = (Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_F");
+				Integer NEW_TARGET_SIGHTED_T = (Integer) this._internal_vars.getVariable("NEW_TARGET_SIGHTED_T");
+				if("CURRENT".equals(TARGET_DESCRIPTION) && NEW_TARGET_SIGHTED_F == 0 && NEW_TARGET_SIGHTED_T == 0) {
 					//randomly choose whether to use a true or false sighting
 					int randInt = (int) (Math.random() * 10);
 					if(randInt >= 5){
