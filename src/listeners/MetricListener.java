c28276192df0295cadfa6c4ab30a171dfa67e517
rob <ivier@studentbody.byu.edu>
2013-08-26 15:19:24 -0600

    metric listener update

--- src/listeners/MetricListener.java
+++ src/listeners/MetricListener.java
@@ -9,7 +9,18 @@ public class MetricListener extends ListenerAdapter {
 	public void executeInstruction ( VM vm, ThreadInfo ti, Instruction insnToExecute ) {
 		MethodInfo mi = insnToExecute.getMethodInfo();
 		String mname = mi.getFullName();
-	    if ( mname.contains("advanceTime") || mname.contains("fire") || mname.contains("getEnabledTransition") )
-	    	System.out.println( mname + " was called." );
+	    if ( mname.contains("advanceTime") ) {
+	    	StackFrame s = ti.getStackFrameExecuting(insnToExecute, 0);
+	    	
+	    	String[] localVarNames = mi.getLocalVariableNames();
+	    	for (int i=0; i<localVarNames.length; i++) {
+	    		LocalVarInfo localVar = mi.getLocalVar(localVarNames[i], 1);
+	    		if(localVar != null){
+	    			System.out.println(localVar.toString());// + ":" + s.getSlot(localVar.getSlotIndex()) + ", ");
+	    		}
+//	    		System.out.print(localVarNames[i]);
+	    	}
+//	    	System.out.print("\n");
+	    }
 	}
 }
